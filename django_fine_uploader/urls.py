88fd4e0f54dc30cc60f571af653305eba183d34d
Derek Adair <d@derekadair.com>
2018-10-17 00:21:41 +0000

    Resolves #9

--- django_fine_uploader/urls.py
+++ django_fine_uploader/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name="django_fine_uploader"
+
 urlpatterns = [
     url(r'^upload/$', views.FineUploaderView.as_view(), name='upload'),
 ]
